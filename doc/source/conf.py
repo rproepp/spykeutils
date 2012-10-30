d81fc38d5fdc414efbc6ec61a95eb97c88a74f7b
Robert Pröpper <robert.proepper@googlemail.com>
2012-10-30 15:45:20 +0100

    Missed a mock

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,8 +35,9 @@ except ImportError:
                     'guiqwt.baseplot', 'guiqwt.plot', 'guiqwt.curve',
                     'guiqwt.image', 'guiqwt.tools', 'guiqwt.signals',
                     'guiqwt.config', 'PyQt4', 'PyQt4.QtCore', 'PyQt4.QtGui',
-                    'guidata', 'guidata.configtools', 'quantities',
-                    'scipy', 'scipy.spatial', 'scipy.spatial.distance']
+                    'guidata', 'guidata.configtools', 'guidata.qthelpers',
+                    'quantities', 'scipy', 'scipy.spatial',
+                    'scipy.spatial.distance']
     for mod_name in MOCK_MODULES:
         sys.modules[mod_name] = MagicMock()
 
