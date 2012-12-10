4a72540320213695e15b1ba11282923b3c58996a
Robert Pröpper <robert.proepper@gmail.com>
2012-12-10 16:35:15 +0100

    Update description: Also works with data from simulations...

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ def find_version():
 
 DESC = """Based on the `Neo <http://packages.python.org/neo/>`_ framework,
 spykeutils is a Python library for analyzing and plotting data from
-neurophysiological recordings. It can be used by itself or in conjunction
-with Spyke Viewer, a multi-platform GUI application for navigating
-electrophysiological datasets.
+neurophysiological recordings or simulations. It can be used by itself or
+in conjunction with Spyke Viewer, a multi-platform GUI application for
+navigating electrophysiological datasets.
 
 For more information, see the documentation at
 http://spykeutils.readthedocs.org"""
