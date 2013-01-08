757a9b9fbf9d9eca4fa0edb6ebaf702937b51d05
Jan Gosmann <jan@hyper-world.de>
2013-01-08 11:29:12 +0100

    Fixing pep8 warnings and errors.

--- spykeutils/spike_train_generation.py
+++ spykeutils/spike_train_generation.py
@@ -1,12 +1,12 @@
 
 import neo
 import numpy.random
+import quantities as pq
 import scipy as sp
 
-import quantities as pq
 
-def gen_homogeneous_poisson(rate, t_start = 0 * pq.s, t_stop = None,
-        max_spikes=None):
+def gen_homogeneous_poisson(
+        rate, t_start=0 * pq.s, t_stop=None, max_spikes=None):
     """ Generate a homogeneous Poisson spike train. The length is controlled
     with `t_stop` and `max_spikes`. Either one or both of these arguments have
     to be given.
@@ -28,26 +28,23 @@ def gen_homogeneous_poisson(rate, t_start = 0 * pq.s, t_stop = None,
     :rtype: SpikeTrain
     """
 
-    max_duration = t_stop - t_start if t_stop != None else None
-
-    if t_stop == None and max_spikes == None:
+    if t_stop is None and max_spikes is None:
         raise ValueError('Either t_stop or max_spikes has to be set.')
 
-    if max_spikes != None:
+    if max_spikes is not None:
         spike_times = sp.cumsum(numpy.random.exponential(
-                rate ** -1, max_spikes)) * (rate.units ** -1).simplified
+            rate ** -1, max_spikes)) * (rate.units ** -1).simplified
         spike_times += t_start
-        if t_stop != None:
+        if t_stop is not None:
             spike_times = spike_times[spike_times <= t_stop]
     else:
         scale = (rate ** -1).rescale(t_stop.units)
         spike_times = [t_start]
         while spike_times[-1] <= t_stop:
-            spike_times.append(spike_times[-1]
-                    + numpy.random.exponential(scale) * scale.units)
+            spike_times.append(
+                spike_times[-1] + numpy.random.exponential(scale) * scale.units)
         spike_times = spike_times[1:-1] * scale.units
 
-    if t_stop == None:
+    if t_stop is None:
         t_stop = spike_times[-1]
     return neo.SpikeTrain(spike_times, t_start=t_start, t_stop=t_stop)
-
--- spykeutils/tests/test_spike_train_generation.py
+++ spykeutils/tests/test_spike_train_generation.py
@@ -1,6 +1,6 @@
 try:
     import unittest2 as ut
-    assert ut # Suppress pyflakes warning about redefinition of unused ut
+    assert ut  # Suppress pyflakes warning about redefinition of unused ut
 except ImportError:
     import unittest as ut
 
@@ -14,7 +14,6 @@ class CommonSpikeTrainGeneratorTests(object):
     """ Provides some common test cases which should work for all spike train
     generation functions.
     """
-
 
     def __init__(self, genFunc, defaultRate, lowRate, highRate):
         """
@@ -34,30 +33,27 @@ class CommonSpikeTrainGeneratorTests(object):
         self.lowRate = lowRate
         self.highRate = highRate
 
-
     def test_returns_SpikeTrain_containing_spikes(self):
         st = self.genFunc(self.defaultRate, max_spikes=10)
         self.assertIsInstance(st, neo.SpikeTrain)
         self.assertTrue(st.size > 0)
 
-
     def test_exception_without_end_condition(self):
         with self.assertRaises(ValueError):
             self.genFunc(self.defaultRate, t_stop=None, max_spikes=None)
-
 
     def test_num_spikes_limited_by_max_spike(self):
         max_spikes = 10
 
         # Use a high rate to provoke more than `max_spike` spikes.
-        self.assertEqual(max_spikes,
+        self.assertEqual(
+            max_spikes,
             self.genFunc(self.highRate, max_spikes=max_spikes).size)
 
         # Use a long trial length to provoke more than `max_spike` spikes.
-        self.assertTrue(max_spikes >=
-            self.genFunc(self.lowRate, t_stop=10000 * pq.s,
-                max_spikes=max_spikes).size)
-
+        self.assertTrue(
+            max_spikes >= self.genFunc(
+                self.lowRate, t_stop=10000 * pq.s, max_spikes=max_spikes).size)
 
     def test_times_limited_by_t_start_and_t_stop(self):
         t_start = 10 * pq.s
@@ -70,24 +66,21 @@ class CommonSpikeTrainGeneratorTests(object):
         self.assertEqual(t_stop, st.t_stop)
 
         # Use a high value for max_spike to not hit that limit
-        st = self.genFunc(self.lowRate, t_start=t_start, t_stop=t_stop,
-                max_spikes=10000)
+        st = self.genFunc(
+            self.lowRate, t_start=t_start, t_stop=t_stop, max_spikes=10000)
         self.assertTrue(sp.all(t_start < st))
         self.assertTrue(sp.all(st <= t_stop))
         self.assertEqual(t_start, st.t_start)
         self.assertEqual(t_stop, st.t_stop)
 
 
-
 class Test_gen_homogeneous_poisson(ut.TestCase, CommonSpikeTrainGeneratorTests):
     def __init__(self, methodName='runTest'):
         ut.TestCase.__init__(self, methodName)
-        CommonSpikeTrainGeneratorTests.__init__(self,
-                stg.gen_homogeneous_poisson, 10 * pq.Hz, 1 * pq.Hz,
-                10000 * pq.Hz)
-
+        CommonSpikeTrainGeneratorTests.__init__(
+            self, stg.gen_homogeneous_poisson, 10 * pq.Hz, 1 * pq.Hz,
+            10000 * pq.Hz)
 
 
 if __name__ == '__main__':
     ut.main()
-
