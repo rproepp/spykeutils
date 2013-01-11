c26f8a0d2e3ba148f6ee06ed54059963267b8611
Jan Gosmann <jan@hyper-world.de>
2013-01-11 16:40:33 +0100

    Improve st_convolve and extract binning method.

--- spykeutils/signal_processing.py
+++ spykeutils/signal_processing.py
@@ -96,28 +96,22 @@ class RectangularKernel(Kernel):
         return (-self.params['half_width'], self.params['half_width'])
 
 
-def _pq_arange(start, stop=None, step=1):
-    if stop is None:
-        stop = start
-        start = 0 * stop.units
-    return sp.arange(
-        start.rescale(stop.units), stop, step.rescale(stop.units)) * stop.units
+def bin_spike_train(train, sampling_rate=None, t_start=None, t_stop=None):
+    """ Creates a binned representation of a spike train.
 
-
-def st_convolve(train, kernel, sampling_rate=None):
-    """ Convolves a spike train with a kernel.
-
-    :param SpikeTrain train: Spike train to convolve.
-    :param function kernel: The kernel function to convolve with. It has to
-        accept a `Quantity 1D` as first argument giving the time points at which
-        the kernel will be evaluated.
-    :param sampling_rate: The sampling rate which will be used to discretize
+    :param SpikeTrain train: Spike train to bin.
+    :param sampling_rate: The sampling rate which will be used to bin
         the spike train. If `None`, `train.sampling_rate` will be used. If that
         is also `None`, :py:const:`default_sampling_rate` will be used.
     :type sampling_rate: Quantity scalar
-    :param kwargs: Additional arguments passed to the kernel function.
-    :returns: The convolved spike train, the boundaries of the discretization
-        bins
+    :param t_start: Time point of the left boundary of the first bin. If `None`,
+        `train.t_start` will be used.
+    :type t_start: Quantity scalar
+    :param t_stop: Timp point of the right boundary of the last bin. If `None`,
+        `train.t_stop` will be used.
+    :type t_stop: Quantity scalar
+    :returns: The binned representation of the spike train, the boundaries of
+        the discretization bins
     :rtype: (1D array, Quantity 1D)
     """
 
@@ -126,12 +120,47 @@ def st_convolve(train, kernel, sampling_rate=None):
             sampling_rate = train.sampling_rate
         else:
             sampling_rate = default_sampling_rate
+    if t_start is None:
+        t_start = train.t_start
+    if t_stop is None:
+        t_stop = train.t_stop
 
     duration = train.t_stop - train.t_start
     num_bins = sampling_rate * duration + 1
-    t_step = duration / num_bins
     bins = sp.linspace(train.t_start, train.t_stop, num_bins)
     binned, _ = sp.histogram(train.rescale(bins.units), bins)
-    k = kernel(_pq_arange(-duration, duration, t_step))
+    return binned, bins
+
+
+def _pq_arange(start, stop=None, step=1):
+    if stop is None:
+        stop = start
+        start = 0 * stop.units
+    return sp.arange(
+        start.rescale(stop.units), stop, step.rescale(stop.units)) * stop.units
+
+
+def st_convolve(
+        train, kernel, kernel_area_fraction=0.999, **discretizationParams):
+    """ Convolves a spike train with a kernel.
+
+    :param SpikeTrain train: Spike train to convolve.
+    :param function kernel: The kernel function to convolve with. It has to
+        accept a `Quantity 1D` as first argument giving the time points at which
+        the kernel will be evaluated.
+    :param discretizationParams: Additional discretization arguments which will
+        be passed to func:`bin_spike_train`.
+    :returns: The convolved spike train, the boundaries of the discretization
+        bins
+    :rtype: (1D array, Quantity 1D)
+    """
+
+    binned, bins = bin_spike_train(train, **discretizationParams)
+    sampling_rate = bins.size / (bins[-1] - bins[0])
+    t_step = 1.0 / sampling_rate
+    t_start, t_stop = kernel.interval_enclosing_at_least(kernel_area_fraction)
+    t_start = sp.ceil(t_start / t_step) * t_step
+    t_stop = sp.floor(t_stop / t_step) * t_step + t_step
+    k = kernel(_pq_arange(t_start, t_stop, t_step))
     k /= (t_step * sp.sum(k))
     return scipy.signal.convolve(binned, k, 'same'), bins
--- spykeutils/tests/test_signal_processing.py
+++ spykeutils/tests/test_signal_processing.py
@@ -107,11 +107,10 @@ class Test_st_convolve(ut.TestCase):
 
     def test_returns_convolved_spike_train(self):
         st = neo.SpikeTrain(sp.array([1.0, 2.0]) * pq.s, t_stop=3.0 * pq.s)
-        kernel = sigproc.Kernel(
-            sigproc.RectangularKernel(0.3 * pq.s))
+        kernel = sigproc.RectangularKernel(0.3 * pq.s)
         expected = sp.array(
-            [0.0, 0.0, 0.0, 0.0, 1.4444444, 1.4444444, 1.4444444, 0.0,
-             1.4444444, 1.4444444, 1.4444444, 0.0])
+            [0.0, 0.0, 0.0, 1.4444444, 1.4444444, 1.4444444, 0.0,
+             1.4444444, 1.4444444, 1.4444444, 0.0, 0.0])
         actual, _ = sigproc.st_convolve(st, kernel, sampling_rate=4 * pq.Hz)
         assert_array_almost_equal(expected, actual)
 
