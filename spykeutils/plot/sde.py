658188632ddfc7e01a4cbe49ca53e66f93d477dd
Robert Pröpper <robert.proepper@gmail.com>
2013-04-24 14:44:25 +0200

    Removed old API compatibility hack

--- spykeutils/plot/sde.py
+++ spykeutils/plot/sde.py
@@ -62,14 +62,6 @@ def sde(trains, events=None, start=0 * pq.ms, stop=None,
     if not progress:
         progress = ProgressIndicator()
 
-        # Catch old API call to remain compatible and make Debian happy.
-        # Can be removed in 0.3.0
-        if isinstance(kernel_function, pq.Quantity):
-            if isinstance(time_unit, ProgressIndicator):
-                progress = time_unit
-            time_unit = kernel_function
-            kernel_function = None
-
     start.units = time_unit
     if stop:
         stop.units = time_unit
