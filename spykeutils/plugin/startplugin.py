af825f3e0b590150d45c40a063f79ef9842f5bef
Robert Pröpper <robert.proepper@gmail.com>
2013-07-23 12:35:28 +0200

    Support GUI progress bar for plugins if PyQt is available

--- spykeutils/plugin/startplugin.py
+++ spykeutils/plugin/startplugin.py
@@ -14,6 +14,57 @@ from spykeutils.plugin import io_plugin
 # Data provider implementations need to be imported so they can be loaded
 import spykeutils.plugin.data_provider_stored
 
+try:
+    from spykeutils.plot.helper import _needs_qt, ProgressIndicatorDialog
+    from PyQt4.QtGui import QApplication
+    _needs_qt.app = QApplication([])
+    progress = ProgressIndicatorDialog(None)
+except ImportError:
+    try:  # Use command line progressbar if package is available
+        import progressbar
+
+        class ProgressIndicatorConsole(ProgressIndicator):
+            """ Implements a progress indicator for the CLI """
+            def __init__(self):
+                widgets = ['', progressbar.Percentage(), ' ',
+                           progressbar.Bar(), ' ', progressbar.ETA()]
+                self.bar = progressbar.ProgressBar(widgets=widgets)
+                self.tick = 0
+                self.maxval = 0
+
+            def set_ticks(self, ticks):
+                """ Set the required number of ticks before the operation is
+                done """
+                self.bar.maxval = ticks
+                self.maxval = ticks
+                self.tick = 0
+
+            def begin(self, title=''):
+                """ Signal that the operation starts """
+                self.tick = 0
+                self.bar.finished = False
+                self.bar.widgets[0] = title + ' '
+                self.bar.start()
+
+            def step(self, num_steps=1):
+                """ Signal that one or more steps of the operation were
+                completed """
+                self.tick += num_steps
+                if self.tick > self.bar.maxval:
+                    self.bar.maxval = self.tick
+                self.bar.update(self.tick)
+                super(ProgressIndicatorConsole, self).step(num_steps)
+
+            def set_status(self, newStatus):
+                """ Set status description """
+                self.bar.widgets[0] = newStatus + ' '
+
+            def done(self):
+                """ Signal that the operation is done. """
+                self.bar.finish()
+        progress = ProgressIndicatorConsole()
+    except ImportError:
+        progress = ProgressIndicator()
 
 def main():
     parser = argparse.ArgumentParser(description='Start an analysis plugin')
@@ -79,7 +130,6 @@ def main():
         plugin.set_parameters(pickle.loads(args.config))
 
     # Load selection
-    sels = []
     try:
         if args.selectionfile:
             f = open(args.Selection, 'r')
@@ -88,7 +138,7 @@ def main():
             sel_string = args.Selection
 
         sels = json.loads(sel_string)
-    except Exception, e:
+    except Exception:
         sys.stderr.write('Could not load selection, aborting...\n')
         sys.exit(1)
 
@@ -98,6 +148,8 @@ def main():
 
     if args.datadir and os.path.isdir(args.datadir):
         AnalysisPlugin.data_dir = args.datadir
+
+    selections[0].progress = progress
     plugin.start(selections[0], selections[1:])
 
 if __name__ == '__main__':
